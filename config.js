346076b92d90e8d29e4e700d97ebe9b742a7138a
hoangngocduan28 <94440550+hoangngocduan28@users.noreply.github.com>
2021-11-16 15:59:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    titleWeb: "demo",
+    introTitle: 'Hoàng Hà Vy',
+    introDesc: `xin lỗi bạn rất nhiều`,
+    btnIntro: '^^tui tệ quá^^',
+    title: 't xin lỗi vì những gì t đã làm sai',
+    desc: 'hãy tha thứ cho t nhé! ',
+    btnYes: 'xem xét thái độ ',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'nói gì với t đi',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    reply: 't muốn kêu tên Vy lắm rồi',
+    mess: 'Mình biết mà. ai cho mà kêu',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
